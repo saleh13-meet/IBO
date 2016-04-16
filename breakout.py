0ef13e90cf757cb24ff7dc3a363afb62c8699b3d
zivhadad1 <zivhadad1@gmail.com>
2016-04-16 16:48:52 +0300

    Updated, game working

--- breakout.py
+++ breakout.py
@@ -2,7 +2,7 @@ from turtle import *
 import turtle
 import random
 import subprocess
- 
+
 
 cmd = ['xrandr']
 cmd2 = ['grep', '*']
@@ -18,9 +18,11 @@ width, height = int(width), int(height)
 
 # doritos.gif -> 50 x 77
 # mountain_dew.gif -> 50 x 82
+# mountain_dew_platform.jpg -> 144 x 46 
  
 doritos_size = [50, 77]
 mountain_dew_size = [50, 82]
+platform_size = [144, 46]
 
 tracer(0)
 screen = Screen()
@@ -32,53 +34,29 @@ SCREEN_HEIGHT = canvas.winfo_height()/2 # here we get the canvas(screen) height
 brick_width = 4
 brick_height = 2
 
-platform_size = [96, 24]
-
 doritos = "Images/doritos.gif"
 mountain_dew = "Images/mountain_dew.gif"
+platform_image= "Images/mountain_dew_platform.gif"
 
 turtle.addshape(doritos)
 turtle.addshape(mountain_dew)
+turtle.addshape(platform_image)
 
 ht()
 
 setup(width, height)
-
-class Doritos():
-	def __init__(self, x, y, hp):
-		self.brick = Brick(x, y, doritos_size[0] , doritos_size[1], doritos)
-		self.width = doritos[0]
-		self.height = doritos[1]
-
-class MountainDew():
-	def __init__(self, x, y ,hp):
-		self.brick = Brick(x, y, mountain_dew_size[0] , mountain_dew_size[1], mountain_dew, hp)
-		self.width = mountain_dew_size[0]
-		self.height = mountain_dew_size[1]
-
-class Platform():
-	def __init__(self, x, y):
-		self.brick = Brick(x, y + platform_size[1], platform_size[1] / 12, platform_size[0] / 12, "square", True)
-		self.width = platform_size[0]
-		self.height = platform_size[1]
-
-	def Move(self, acc_x):
-		self.brick.goto(self.brick.xcor() + acc_x, self.brick.ycor())
-		self.brick.move()
 
 class Brick(Turtle):
 	def __init__(self, x, y, width, height, shape, hp = 1, is_platform = False):
 		Turtle.__init__(self)
 		self.is_platform = is_platform
 		self.hp = hp
-		self.shapesize(width, height)
 		self.shape(shape)
 		self.showturtle()
 		self.penup()
 		self.goto(x,y)
 		self.width = width
 		self.height = height
-
 
 	def move(self):
 		self.goto(self.xcor(),self.ycor())
@@ -98,11 +76,36 @@ class Brick(Turtle):
 	def Hit(self):
 		self.hp -= 1
 		if self.hp <= 0 and not self.is_platform:
-			Destroy()
+			self.Destroy()
+		if (self.hp == 1):
+			self.shape(doritos)
 
 	def Destroy(self):
 		self.ht()
 		self.goto(9999,9999)
+
+class Doritos(Brick):
+	def __init__(self, x, y, hp):
+		Brick.__init__(self, x, y, doritos_size[0] , doritos_size[1], doritos, hp)
+		self.width = doritos_size[0]
+		self.height = doritos_size[1]
+
+class MountainDew(Brick):
+	def __init__(self, x, y ,hp):
+		Brick.__init__(self, x, y, mountain_dew_size[0] , mountain_dew_size[1], mountain_dew, hp)
+		self.width = mountain_dew_size[0]
+		self.height = mountain_dew_size[1]
+
+class Platform(Brick):
+	def __init__(self, x, y):
+		Brick.__init__(self, x, y, platform_size[1], platform_size[0], platform_image, 9999,True)
+		self.width = platform_size[0]
+		self.height = platform_size[1]
+
+	def Move(self, acc_x):
+		self.goto(self.xcor() + acc_x, self.ycor())
+		self.move()
+
 
 
 class Ball(Turtle):
@@ -116,6 +119,7 @@ class Ball(Turtle):
 		self.penup()
 		self.shape("circle")
 		self.ht()
+		self.last_brick = None
 	
 	def goto(self,x,y):
 		super(Ball,self).goto(x,y)
@@ -160,22 +164,33 @@ class Ball(Turtle):
 
 	def HitBricks(self, bricks):
 		center_x = self.xcor()
-		center_y = self.ycor()		
-		for i in bricks:
+		center_y = self.ycor()
+		for brick in bricks:
 			hit = False
 			left = brick.left_side()
 			right = brick.right_side()
 			bottom = brick.bottom_side()
 			top = brick.top_side()
 
-			if right >= self.left_side() or left <= self.right_side() and center_y <= self.top_side() and center_y >= self.bottom_side():
-				self.Struck("y")
-				hit = True
-			
-			if top >= self.bottom_side() or bottom <= self.top_side() and center_x <= self.right_side() and center_x >= self.left_side():
-				self.Struck("x")
-				hit = True
+			if self.top_side() < top and self.bottom_side() > bottom:
+				if self.right_side() > left and self.left_side() < right:
+					hit = True
+					struck = "x"
 
-			if hit:
+				elif self.left_side() < right and self.right_side() > left:
+					hit = True
+					struck = "x"
+
+			elif self.right_side() < right and self.left_side() > left:
+				if self.top_side() > bottom and self.bottom_side() < top:
+					hit = True
+					struck = "y"
+
+				elif self.bottom_side() < top and self.top_side() > bottom:
+					hit = True
+					struck = "y"
+
+			if hit and self.last_brick is not brick:
 				brick.Hit()
-
+				self.last_brick = brick
+				self.Struck(struck)
--- game.py
+++ game.py
@@ -5,10 +5,7 @@
 from breakout import *
 import turtle
 
-def Left():
-	platform.Move(-15)
-def Right():
-	platform.Move(15)
+
 
 left = -width / 2 + 30
 right = width / 2
@@ -22,23 +19,42 @@ bricks_types = [Doritos, MountainDew]
 border_spacing = [40, 50]
 bricks_spacing = 5
 
-platform = Platform(0, bottom)
-ball = Ball(0,0, random.randint(-10**2, 10**2)/(10.0**2), -random.randint(0,10**2)/(10.0**2), 12, "red")
+platform = Platform(0, bottom + 20)
+balls = [Ball(0,0, random.randint(-10**2, 10**2)/(10.0**2), -random.randint(0,10**2)/(10.0**2), 10, "red")]
+
+platform_dx = 30
+def Left():
+	platform.Move(-platform_dx)
+def Right():
+	platform.Move(platform_dx)
+def SetMousePosition():
+	platform.Move(mouse_position()[0])
 
 onkey(Left, "Left")
 onkey(Right, "Right")
-
 
 for x in range(left + border_spacing[0], right - border_spacing[0], doritos_size[0] + bricks_spacing):
 	for y in range(top - border_spacing[1], 0, -(mountain_dew_size[1] + bricks_spacing)):
 		a = random.randint(0, 1)
 		bricks.append(bricks_types[a](x, y, a + 1))
 
+colors = ["green", "blue", "purple", "pink", "orange", "brown"]
+
+bricks.append(platform)
 listen()
 
+total_bricks = len(bricks)
+
 while True:
-	ball.move()
-	ball.Borders(left, right - 15, top - 25, bottom)
+	for ball in balls:
+		ball.move()
+		ball.Borders(left, right - 15, top - 25, bottom)
+		ball.HitBricks(bricks)
+		if len(balls) / float(total_bricks) / 4.0 % 1 == 0:
+			print "ANOTHER BALL " - len(balls) + 1
+			color = random.choice(colors)
+			colors.remove(color)
+			balls.append(Ball(-width + 50,0, random.randint(0.1, 0.5), -random.randint(0.1, 0.5), color))
 	update()
 
 mainloop()
