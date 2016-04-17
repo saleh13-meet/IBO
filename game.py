d27009d331788a11a10d97784f19dbe1eed155c7
hashem <hashem13@meet.mit.edu>
2016-04-17 14:50:15 +0300

    Fixed bugs, *pressing spacebar adds another ball!

--- game.py
+++ game.py
@@ -17,7 +17,7 @@ bricks = []
 bricks_types = [Doritos, MountainDew]
 
 border_spacing = [40, 50]
-bricks_spacing = 5
+bricks_spacing = 10
 
 platform = Platform(0, bottom + 20)
 balls = [Ball(0,0, random.randint(-10**2, 10**2)/(10.0**2), -random.randint(0,10**2)/(10.0**2), 10, "red")]
@@ -27,11 +27,15 @@ def Left():
 	platform.Move(-platform_dx)
 def Right():
 	platform.Move(platform_dx)
+def Space():
+	global to_add_ball
+	to_add_ball = True
 def SetMousePosition():
 	platform.Move(mouse_position()[0])
 
 onkey(Left, "Left")
 onkey(Right, "Right")
+onkey(Space, "space")
 
 for x in range(left + border_spacing[0], right - border_spacing[0], doritos_size[0] + bricks_spacing):
 	for y in range(top - border_spacing[1], 0, -(mountain_dew_size[1] + bricks_spacing)):
@@ -45,16 +49,19 @@ listen()
 
 total_bricks = len(bricks)
 
+to_add_ball = False
+
 while True:
 	for ball in balls:
 		ball.move()
 		ball.Borders(left, right - 15, top - 25, bottom)
 		ball.HitBricks(bricks)
-		if len(balls) / float(total_bricks) / 4.0 % 1 == 0:
-			print "ANOTHER BALL " - len(balls) + 1
-			color = random.choice(colors)
-			colors.remove(color)
-			balls.append(Ball(-width + 50,0, random.randint(0.1, 0.5), -random.randint(0.1, 0.5), color))
+	
+	if to_add_ball:
+		color = random.choice(colors)
+		colors.remove(color)
+		balls.append(Ball(0,-height / 2 + 30, random.randint(5, 10) / 10.0, random.randint(5, 10) / 10.0, 10, color))
+		to_add_ball = False
 	update()
 
 mainloop()
